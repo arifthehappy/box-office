97ab107d21b4c53a8387dab617d01a278b03dc9c
MD ARIF KHAN <55360458+arifthehappy@users.noreply.github.com>
2022-07-20 21:55:12 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-    hi
-    </div>
+    <Switch>
+      <Route exact path="/">
+        This is a home page
+      </Route>
+
+      <Route exact path="/starred">
+        This is starred
+      </Route>
+
+      <Route>This is 404 not found</Route>
+    </Switch>
   );
 }
 
